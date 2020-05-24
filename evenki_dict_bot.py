b24da5d1e3f01d05af185854af69ded80e802428
gisly <gisly@list.ru>
2020-05-24 13:45:43 +0300

    added logging

--- evenki_dict_bot.py
+++ evenki_dict_bot.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
+import pickle
+import random
+
 import telebot
 __author__ = "gisly"
 
 bot = telebot.TeleBot('1082882518:AAHB7pDqCCPq7OCBHMtgT28eT-1huop_cXw')
-evenki_dict = {'олень': ['орон', 'багдака'],
-               'волк': ['иргичи']
-               }
+evenki_dict = None
+REASONS_NO_WORD = ['К сожалению, такого слова я не знаю.',
+                   'Увы :( Попробуй что-нибудь еще.',
+                   'Хм, в следующий раз попробую найти.',
+                   'Впервые слышу такое слово. Что это?']
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -15,11 +20,26 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    evenki_words = evenki_dict.get(message.text)
+    initialize_dict()
+    evenki_words = evenki_dict.get(message.text.strip())
     if not evenki_words:
-        bot.send_message(message.chat.id, 'Такого слова я не знаю. Попробуй поискать что-нибудь другое.')
+        evenki_words = evenki_dict.get(message.text.strip().lower())
+    if not evenki_words:
+        bot.send_message(message.chat.id, get_random_reason())
     else:
-        bot.send_message(message.chat.id, 'Вот какие переводы нашлись: *' + ', '.join(evenki_words) + '*',
+        bot.send_message(message.chat.id, 'Вот какие переводы нашлись: *' + ', '.join(sorted(evenki_words)) + '*',
                          parse_mode='Markdown')
+
+
+def initialize_dict():
+    global evenki_dict
+    if evenki_dict is None:
+        with open('vasilevich.pickle', 'rb') as handle:
+            evenki_dict = pickle.load(handle)
+    print('dict initialized')
+
+def get_random_reason():
+    random_num = random.randint(0,  len(REASONS_NO_WORD) - 1)
+    return REASONS_NO_WORD[random_num]
 
 bot.polling()
